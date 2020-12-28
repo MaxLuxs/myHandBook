66d09e79b190edb3f6848f5577f8b860c58199d2
Maksim <max.1172.luk@gmail.com>
2020-12-28 22:03:19 +0300

    Create Relationships for section and References.

--- app/src/main/java/by/tms/myhandbook/Model/HandbookDatabase.kt
+++ app/src/main/java/by/tms/myhandbook/Model/HandbookDatabase.kt
@@ -6,6 +6,7 @@ import by.tms.myhandbook.Section
 
 @Database(entities = [ References::class, Section::class], version = 1)
 abstract class HandbookDatabase() : RoomDatabase() {
+    abstract fun ReferencesDao(): ReferencesDao
 //    abstract fun sectionDao() : SectionDao
 //    abstract fun referencesDao() : ReferencesDao
 }
